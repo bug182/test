d0b4891b485fc8071dc56798938cacaf8ed08d33
root <root@localhost.localdomain>
2018-11-23 03:57:14 -0800

    Uncommented the ip/port stuff, and put defaults to said ip/port

--- test/Kali_screw_around/networking.py
+++ test/Kali_screw_around/networking.py
@@ -1,12 +1,12 @@
 import socket
 s = socket.socket()
 
-#ip = input('What ip do you want?: ')
-#port = int(input('what port do you want?:'))
-#print(type(ip))
-#print(type(port))
+ip = input('What ip do you want?: ')
+port = int(input('what port do you want?: '))
+print(type(ip))
+print(type(port))
 
-s.connect(('192.168.0.43', 5000))
+s.connect((ip, port))
 
 answer = s.recv(1024)
 print(answer)
